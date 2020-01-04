08d364b9a09f34cebf2592aa9e07a5a13ae89af1
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-01-04 12:12:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  switch (true) {
+    case distance < 400:
+      return 'This one is on me!';
+    case distance  > 2000 && distance < 2500:
+      return 'I will gladly take your thirty bucks.';
+    default:
+      return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  return destination === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return "Bye.";
+  } 
 }
